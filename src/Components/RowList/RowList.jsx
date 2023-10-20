da08cd5cc528365dc36b08fb27d35db9ad0dfd02
David <Rigby335@hotmail.com>
2023-10-20 12:05:00 -0700

    ft. dynamic get all res set into state, validated

--- src/Components/RowList/RowList.jsx
+++ src/Components/RowList/RowList.jsx
@@ -1,12 +1,16 @@
 import EditRow from "./EditRow";
 import Row from "./Row";
 import { addRow, getAll } from "../../supabase-utils";
+import { useState } from "react";
 
 export default function RowList() {
+  const [readAll, setReadAll] = useState([]);
   async function handleAddRow(rowObj) {
     await addRow(rowObj);
-    // await getAll();
+    const res = await getAll();
+    setReadAll(res);
   }
+  console.log("readAll", readAll);
   return (
     <div>
       <Row />
