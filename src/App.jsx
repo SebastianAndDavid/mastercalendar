089248a96628688941a6ae78278fee4bd816a9d8
David <Rigby335@hotmail.com>
2023-10-23 16:18:20 -0700

    ft. footer, header, toprow styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import TopRow from "./Components/TopRow/TopRow";
 function App() {
   return (
     <>
-      <header>hi</header>
+      <header>
+        <h1>The Design Roadmap Master Calendar</h1>
+      </header>
       <div className="app">
         <div className="calendar-container">
           <TopRow />
@@ -15,6 +17,31 @@ function App() {
         <div className="card-container">
           <CardList />
         </div>
+        <footer>
+          <nav className="nav-container">
+            <section className="delete">
+              <img
+                src="https://img.icons8.com/?size=50&id=11705&format=png"
+                alt=""
+              />
+              <p>DELETE TABLE</p>
+            </section>
+            <section className="new-table">
+              <img
+                src="https://img.icons8.com/?size=50&id=6697&format=png"
+                alt=""
+              />
+              <p>NEW TABLE</p>
+            </section>
+            <section className="teammember">
+              <img
+                src="https://img.icons8.com/?size=80&id=111473&format=png"
+                alt=""
+              />
+              <p>NEW MEMBER</p>
+            </section>
+          </nav>
+        </footer>
       </div>
     </>
   );
