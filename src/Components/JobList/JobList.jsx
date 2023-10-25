8f0c0a7e04e7f0c2052db28712d4ab9a6318c729
Sebastian-Simek <sebastiansimek09@gmail.com>
2023-10-25 15:13:49 -0700

    ft added dynamic insert job function

--- src/Components/JobList/JobList.jsx
+++ src/Components/JobList/JobList.jsx
@@ -1,11 +1,19 @@
+import JobCard from "./JobCard";
 import "./JobList.css";
 
 export default function JobList() {
   return (
     <>
       <div className="job-input">
-          <input placeholder="Job Name" />
+        <input placeholder="Job Name" />
         <button>Add</button>
+      </div>
+      <div className="jobcards-container">
+        <JobCard />
+        <JobCard />
+        <JobCard />
+        <JobCard />
+        <JobCard />
       </div>
     </>
   );
